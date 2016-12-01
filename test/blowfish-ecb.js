288efa0f352700e8bfacc587de14802bf5a66ce3
Tugrul Topuz <tugrultopuz@destekmenkul.com.tr>
2016-12-01 09:58:21 +0300

    Blowfish ECB Test Case

--- test/blowfish-ecb.js
+++ test/blowfish-ecb.js
@@ -20,7 +20,7 @@ describe('MCrypt instance (BLOWFISH-ECB)', function() {
                 mc.open(new Buffer(key, 'base64'));
             }, 'there is error when opened with key');
         });
-        
+
         describe('encrypt', function() {
             it('plaintext and decrypted ciphertext should be same', function(){
                 assert.equal(ciphertext, mc.encrypt(plaintext).toString('base64'), 'ciphertext are not same');
@@ -30,6 +30,7 @@ describe('MCrypt instance (BLOWFISH-ECB)', function() {
 
         describe('decrypt', function() {
             it('ciphertext and encrypted plaintext should be same', function(){
+                var result = mc.decrypt(new Buffer(ciphertext, 'base64')).toString();
                 assert.equal(plaintext, result, 'plaintext are not same');
             });
         });
